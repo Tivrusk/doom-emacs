b6b1542c0c34121d2af3fa0e71925ce248daafb3
Tristan Großkopf <tristan.grosskopf@stud.uni-goettingen.de>
2024-01-21 09:35:01 +0100

    Fullscreen, org (template) and banner.

--- config.el
+++ config.el
@@ -46,6 +46,38 @@
 (map!   :nvi "C-+" #'text-scale-increase
         :nvi "C-=" #'doom/reset-font-size)
 
+(toggle-frame-fullscreen)
+
+(defun my-weebery-is-always-greater ()
+  (let* ((banner '("Yo!"))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                +doom-dashboard--width
+                (concat line (make-string (max 0 (- longest-line (length line))) 32)))
+               "\n"))
+     'face 'doom-dashboard-banner)))
+
+(setq +doom-dashboard-ascii-banner-fn #'my-weebery-is-always-greater)
+
+(after! org (setq org-hide-emphasis-markers t))
+(after! org (setq org-insert-heading-respect-content nil))
+(after! org
+  (setq org-log-done t)
+  (setq org-log-into-drawer t))
+
+(after! org
+  (add-to-list 'org-capture-templates
+               '("k" "Konspekt Zeile" plain (file "") ;; use with C-0
+                 "%[~/0_Org/Templates/konspekt_zeile.org]"
+                 :empty-lines-before 0
+                 :empty-lines-after 0
+                 :jump-to-captured t
+                 :immediate-finish t)
+               ))
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
@@ -77,3 +109,4 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
